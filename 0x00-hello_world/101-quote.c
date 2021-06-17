cb2bd1a9591fd933336b67bb48dfeb3db67255a0
BORNALMIGHTY <84774543+BORNALMIGHTY@users.noreply.github.com>
2021-06-17 10:57:38 -0400

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,9 +5,9 @@
  *
  * Return: Always 0 (Success)
  */
-int main()
+int main(void)
 {
-	write("and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
 
 	return (1);
 }
